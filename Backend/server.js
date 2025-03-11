adcc03a478ecfbd24764d205ba9ca383ac7e5409
H. Hiranya Jayasundera <hhansi2002jayesundera@gmail.com>
2025-03-12 01:55:54 +0530

    Initial Commit
    Added the folder structure

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "../Backend/DB/db.js";
+
+dotenv.config(); 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB(); 
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("MongoDB Connection Successful!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
